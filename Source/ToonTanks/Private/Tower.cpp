7edde7e819854d6467e69175e62486410498ef16
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2024-09-01 19:40:53 +0100

    Define and implement the default constructor, BeginPlay() and Tick().

--- Source/ToonTanks/Private/Tower.cpp
+++ Source/ToonTanks/Private/Tower.cpp
@@ -2,4 +2,24 @@
 
 
 #include "Tower.h"
+#include "Tank.h"
 
+ATower::ATower()
+{
+	Tank = nullptr;
+}
+
+void ATower::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector const LookAtTarget = Tank->GetActorLocation();
+	RotateTurret(LookAtTarget, DeltaTime);
+}
+
+void ATower::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Tank = Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+}
