6af3bfc59db7ae36e12d7573c768bc1a5fac0f87
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2024-09-05 14:20:56 +0100

    Delete Tick().

--- Source/ToonTanks/Private/Projectile.cpp
+++ Source/ToonTanks/Private/Projectile.cpp
@@ -7,7 +7,7 @@
 AProjectile::AProjectile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	// allocate memory for components
 	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh"));
@@ -18,13 +18,4 @@ AProjectile::AProjectile()
 void AProjectile::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
-
-// Called every frame
-void AProjectile::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
