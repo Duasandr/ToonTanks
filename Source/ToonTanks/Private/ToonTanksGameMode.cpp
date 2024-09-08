dcf2ab6568875c18333b7c10fad33edfc238d773
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2024-09-08 15:34:14 +0100

    Implement ActorDied().

--- Source/ToonTanks/Private/ToonTanksGameMode.cpp
+++ Source/ToonTanks/Private/ToonTanksGameMode.cpp
@@ -2,10 +2,25 @@
 
 
 #include "ToonTanksGameMode.h"
+#include "Tank.h"
+#include "Tower.h"
+#include "Kismet/GameplayStatics.h"
 
 void AToonTanksGameMode::ActorDied(AActor* DeadActor)
 {
-	
+	if (DeadActor == Tank)
+	{
+		Tank->HandleDestruction();
+		if (auto * PlayerController = Tank->GetPlayerController())
+		{
+			Tank->DisableInput(PlayerController);
+			PlayerController->bShowMouseCursor = false;
+		}
+	}
+	else if (auto * Tower = Cast<ATower>(DeadActor))
+	{
+		Tower->HandleDestruction();	
+	}
 }
 
 void AToonTanksGameMode::BeginPlay()
