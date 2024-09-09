e8a19b136b18dfc25566ecc18532b9150fda62b1
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2024-09-09 09:18:04 +0100

    Declare, define and implement SetPlayerEnabled().

--- Source/ToonTanks/Private/ToonTanksPlayerController.cpp
+++ Source/ToonTanks/Private/ToonTanksPlayerController.cpp
@@ -2,4 +2,20 @@
 
 
 #include "ToonTanksPlayerController.h"
+#include "GameFramework/Pawn.h"
 
+void AToonTanksPlayerController::SetPlayerEnabledState(bool const bPlayerEnabled)
+{
+	if (APawn * Pawn = GetPawn())
+	{
+		if (bPlayerEnabled)
+		{
+			Pawn->EnableInput(this);
+		}
+		else
+		{
+			Pawn->DisableInput(this);
+		}
+		bShowMouseCursor = bPlayerEnabled;
+	}
+}
