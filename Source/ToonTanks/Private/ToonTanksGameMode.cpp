70f1ab3d34785bb97817d4939adb56805503ea7d
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2024-09-09 11:22:48 +0100

    Call GameStart() from HandleGameStart().

--- Source/ToonTanks/Private/ToonTanksGameMode.cpp
+++ Source/ToonTanks/Private/ToonTanksGameMode.cpp
@@ -37,6 +37,8 @@ void AToonTanksGameMode::HandleGameStart()
 		UGameplayStatics::GetPlayerController(this, 0)
 		);
 
+	GameStart();
+
 	if (ToonTanksPlayerController)
 	{
 		ToonTanksPlayerController->SetPlayerEnabledState(false);
